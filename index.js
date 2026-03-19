9db0da99cb07a1d6affe9b0b0265fa588252d30f
Tyler Long <aethdae@gmail.com>
2026-03-18 20:59:44 -0500

    initial commit, add pages and logic to fetch pokemon

---
+++ index.js
@@ -0,0 +1,81 @@
+const output = document.getElementById("output");
+const form = document.getElementById("inputForm");
+const selectBox = document.getElementById("generation");
+const inputNum = document.getElementById("inputNum");
+
+async function main() {
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    renderLoading();
+    const pokeNum = parseInt(e.target.inputNum.value);
+    await getPokemonAPI(pokeNum, e.target.generation.value);
+    inputNum.value = "";
+    selectBox.value = "generation-i";
+    removeLoading();
+  });
+}
+
+async function getPokemonAPI(number, generation) {
+  try {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}`);
+    if (res.ok) {
+      const data = await res.json();
+      renderPokemon(data, generation);
+    }
+  } catch (error) {
+    renderError(error);
+    console.log(error);
+  }
+}
+
+function renderLoading() {
+  console.log("I wasn't");
+  const para = document.createElement("p");
+  para.id = "loading";
+  para.innerText = "Loading...";
+  output.appendChild(para);
+}
+
+function removeLoading() {
+  const load = document.getElementById("loading");
+  output.removeChild(load);
+}
+
+function renderError(error) {
+  const para = document.createElement("p");
+  para.innerText = error;
+  output.appendChild(para);
+  setTimeout(() => output.removeChild(para), 2000);
+}
+
+function renderPokemon(data, generation) {
+  if (
+    data.sprites.versions[generation][
+      Object.keys(data.sprites.versions[generation])[0]
+    ]["front_default"]
+  ) {
+    const div = document.createElement("div");
+    const pokeName = document.createElement("p");
+    const pokeLowerName = data.name.slice(1);
+    const pokeUpperName = data.name[0].toUpperCase();
+    pokeName.innerText = pokeUpperName + pokeLowerName;
+
+    console.log(Object.keys(data.sprites.versions[generation]));
+    const img =
+      data.sprites.versions[generation][
+        Object.keys(data.sprites.versions[generation])[0]
+      ]["front_default"];
+
+    const imgElement = document.createElement("img");
+    imgElement.src = img;
+    imgElement.alt = `${data.name} from ${generation}`;
+
+    div.appendChild(imgElement);
+    div.appendChild(pokeName);
+    output.appendChild(div);
+  } else {
+    renderError("Image doesn't exist for this Pokemon in this generation.");
+  }
+}
+
+main();
